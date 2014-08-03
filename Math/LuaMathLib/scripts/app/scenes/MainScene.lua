0f4e84c2ad9e13f685988556ae6aeecc834f7210
Henry.T <tang.hy@live.com>
2014-08-04 02:19:59 +0800

    为博客整理转盘Demo

--- Math/LuaMathLib/scripts/app/scenes/MainScene.lua
+++ Math/LuaMathLib/scripts/app/scenes/MainScene.lua
@@ -4,9 +4,9 @@ local MainScene = class("MainScene", function()
 end)
 
 function MainScene:ctor()
-    self._label = ui.newTTFLabel({text = "Hello, World", size = 20, align = ui.TEXT_ALIGN_CENTER})
-        :pos(100, display.cy)
-        :addTo(self)
+
+    self._bg = display.newSprite("spin/bg.png"):scale(0.5):pos(display.cx, display.cy):addTo(self)
+    self._arrow = display.newSprite("spin/arrow.png"):scale(0.5):pos(display.cx, display.cy+5):addTo(self)
 
     self._vX = 0
     self._posX = 0
@@ -16,13 +16,13 @@ end
 function MainScene:onEnter()
     -- NOTE SmoothDamp测试
     self:addNodeEventListener(cc.NODE_ENTER_FRAME_EVENT, function(dt)
-        --self._posX = self._label:getPositionX()
+        --self._posX = self._arrow:getPositionX()
         --self._posX, self._vX = Math.SmoothDamp(self._posX, 300, self._vX, 1, 1000, dt)
-        --self._label:setPosition(self._posX, self._label:getPositionY())
+        --self._arrow:setPosition(self._posX, self._arrow:getPositionY())
 
-        self._rot = self._label:getRotation()
-        self._rot, self._vX = Math.SmoothDamp(self._rot, 7200, self._vX, 5, 1000, dt)
-        self._label:setRotation(self._rot)
+        self._rot = self._arrow:getRotation()
+        self._rot, self._vX = Math.SmoothDamp(self._rot, 7200, self._vX, 2, 1000, dt)
+        self._arrow:setRotation(self._rot)
     end)
     self:scheduleUpdate()
 end
